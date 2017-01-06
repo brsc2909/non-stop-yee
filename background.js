00411370e2a706dd203b5097fd00376ef9f6b8c4
Brendan Scullion <brsc2909@gmail.com>
2017-01-06 20:04:57 +0000

    initial init

---
+++ background.js
@@ -0,0 +1,14 @@
+function sandstorm(){
+    var myAudio = new Audio("Yee.wav");        // create the audio object
+    //myAudio.src = "sounds/sound.wav"; // assign the audio file to it
+    myAudio.volume=.01;
+    myAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+    }, false);
+    //myAudio.load()
+    myAudio.play(); 
+
+}
+
+sandstorm()
